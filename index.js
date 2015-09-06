3dc36154f2b7496c8a9a1b333a2178fab4354f73
phylp <philson3@gmail.com>
2015-09-06 09:37:47 -0700

    blocking i/o  example complete

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var server = require('./server');
 var router = require('./router');
-var requestHandlers = require('./request_handler');
+var requestHandlers = require('./request_handlers');
 
 var handle = {};
 handle['/'] = requestHandlers.start;
 handle['/start'] = requestHandlers.start;
-handle['upload'] = requestHandlers.upload;
+handle['/upload'] = requestHandlers.upload;
 
 server.start(router.route, handle);
