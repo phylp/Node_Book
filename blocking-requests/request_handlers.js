962600ebd10e1f26b877bd76c11455527d80150b
Phil <philson3@gmail.com>
2015-09-15 20:57:05 -0700

    chapters merged to master

---
+++ blocking-requests/request_handlers.js
@@ -0,0 +1,30 @@
+
+var exec = require('child_process').exec;
+
+function start(){
+  console.log('Request handler --START-- was called');
+  var content = 'empty';
+
+  function sleep(milliSeconds){          //this is basically a simulation of a timeout function
+  var startTime = new Date().getTime();
+  while(new Date().getTime() < startTime + milliSeconds);
+ }
+
+  sleep(8000);                    
+  return 'Hello Start';
+}
+
+
+
+function upload(){
+  console.log('Request handle --UPLOAD-- was called');
+  return 'Hello Upload';
+}
+
+
+exports.start = start;
+exports.upload = upload;
+
+//now when start url is accessed, upload will not be able to be accessed for 8 seconds. 
+//This is blocking i/o. Works, but it isn't good.
+
---
+++ file-upload/router.js
@@ -0,0 +1,18 @@
+function route(handle, pathname, response, request) {     //unlike blocking example, here we added a response object to pass into function
+  console.log('About to route a request for ' + pathname);
+  if(typeof handle[pathname] === 'function'){
+    return handle[pathname](response, request);
+  } else {
+    console.log('No request handler found for ' + pathname);
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.write('404 Not found. Sorry!');
+    response.end();
+  }
+}
+
+exports.route = route;
+
+
+//in this example, unlike the blocking i/o case, we have no return value
+//Instead we are passing the respond object on. It will go on to the request handler.
+
